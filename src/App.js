2dbc5aca77bd88338d339e09e16417c2c26b98f3
Pleasant Codes <hellopleasantcodes@gmail.com>
2020-08-06 16:05:24 -0700

    react route established

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Fragment } from "react";
 import "./App.css";
 
 import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import Singleroom from "./pages/Singleroom";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return <Fragment>hello from app</Fragment>;
+  return (
+    <Fragment>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms/" component={Rooms} />
+      <Route exact path="/rooms/:lit" component={Singleroom} />
+
+      <Error></Error>
+    </Fragment>
+  );
 }
 
 export default App;
