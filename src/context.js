e3ad0b06e87b1d02bc1527876190dd1a473f0b72
Pleasant Codes <hellopleasantcodes@gmail.com>
2020-08-12 19:43:15 -0700

    done with all

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import items from "./data";
+import Client from "./contentful";
 
 const RoomContext = React.createContext();
 //
@@ -21,23 +22,33 @@ export default class RoomProvider extends Component {
     pets: false,
   };
   // get data
+  getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: "beachResortRoom",
+        order: "sys.createdAt",
+      });
+      let rooms = this.formatData(items);
+      let featuredRooms = rooms.filter((room) => room.featured === true);
+      let maxPrice = Math.max(...rooms.map((item) => item.price));
+      let maxSize = Math.max(...rooms.map((item) => item.size));
+
+      this.setState({
+        rooms,
+        featuredRooms,
+        sortedRooms: rooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice,
+        maxSize,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   componentDidMount() {
-    //this.getdata
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter((room) => room.featured === true);
-    let maxPrice = Math.max(...rooms.map((item) => item.price));
-    let maxSize = Math.max(...rooms.map((item) => item.size));
-
-    this.setState({
-      rooms,
-      featuredRooms,
-      sortedRooms: rooms,
-      loading: false,
-      price: maxPrice,
-      maxPrice,
-      maxSize,
-    });
+    this.getData();
   }
 
   formatData(items) {
