667a1aa2c3653a42e8050fabf4fef68288274344
Pleasant Codes <hellopleasantcodes@gmail.com>
2020-08-12 19:48:21 -0700

    leo

--- src/contentful.js
+++ src/contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  space: "57n3s936fome",
+  accessToken: "HWasXmCu3Hnmyk0XC_JWOM_8SlFkgntLT8NogfrE7iw",
 });
